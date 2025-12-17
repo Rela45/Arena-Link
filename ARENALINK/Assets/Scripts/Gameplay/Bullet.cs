eeba3592a0e85201526f6c79157d5dab9b582961
Rela45 <alex.imbriani00@gmail.com>
2025-12-17 17:05:18 +0100

    Implement player movement and shooting mechanics; add bullet prefab and update project settings

--- ARENALINK/Assets/Scripts/Gameplay/Bullet.cs
+++ ARENALINK/Assets/Scripts/Gameplay/Bullet.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        
+        Destroy(gameObject);
     }
 }
--- ARENALINK/Assets/Scripts/Gameplay/PlayerController.cs
+++ ARENALINK/Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,16 +1,36 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Weapon weapon;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+        }
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = MathF.Atan2(aimDirection.y,aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
