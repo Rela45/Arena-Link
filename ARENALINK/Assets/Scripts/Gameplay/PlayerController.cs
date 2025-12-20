e2d107ab0823e93161203a28c18ff06264307ae1
Rela45 <alex.imbriani00@gmail.com>
2025-12-20 13:01:58 +0100

    Add dash mechanics to PlayerController for enhanced movement

--- ARENALINK/Assets/Scripts/Gameplay/PlayerController.cs
+++ ARENALINK/Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,8 +11,14 @@ public class PlayerController : MonoBehaviour
 
     Vector2 moveDirection;
     Vector2 mousePosition;
-    
 
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.15f;
+    public float dashCooldown = 1f;
+
+    bool isDashing = false;
+    float lastDashTime = -999f;
+    Vector2 dashDirection;
     // Update is called once per frame
     void Update()
     {
@@ -24,6 +32,13 @@ public class PlayerController : MonoBehaviour
         moveDirection = new Vector2(moveX, moveY).normalized;
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        if(Input.GetKeyDown(KeyCode.Q) && Time.time >= lastDashTime + dashCooldown)
+        {
+            if(moveDirection.sqrMagnitude > 0.01f)
+            {
+                dashDirection = moveDirection;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -32,5 +47,18 @@ public class PlayerController : MonoBehaviour
         Vector2 aimDirection = mousePosition - rb.position;
         float aimAngle = MathF.Atan2(aimDirection.y,aimDirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = aimAngle;
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        lastDashTime = Time.time;
+        float t = 0f;
+        while(t < dashDuration)
+        {
+            t += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        isDashing = false;
     }
 }
