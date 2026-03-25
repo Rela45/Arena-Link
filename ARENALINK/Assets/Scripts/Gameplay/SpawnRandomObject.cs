a1d99bedfb1691c9898956af95b3d9e11c415b3f
Rela45 <alex.imbriani00@gmail.com>
2026-03-25 18:30:03 +0100

    Refactor SpawnRandomObject script to use an array of debris prefabs and clean up unused code

--- ARENALINK/Assets/Scripts/Gameplay/SpawnRandomObject.cs
+++ ARENALINK/Assets/Scripts/Gameplay/SpawnRandomObject.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public GameObject debrisPrefab;
     
-
-    
+    public GameObject[] debriPrefabs;
     void Update()
     {
-        
+        Vector2 randomSpawn = new Vector2(Random.Range(-110, 11), Random.Range(-11, 10));
     }
 }
