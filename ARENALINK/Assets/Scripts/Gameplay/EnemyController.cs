409e88dff0ce6f39b30289a1a4480fd786c24f23
Rela45 <alex.imbriani00@gmail.com>
2026-04-11 01:47:36 +0200

    Add raycast function to make an enemy line of sight

---
+++ ARENALINK/Assets/Scripts/Gameplay/EnemyController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Raycast Debug")]
+    public float rayLength = 25f;
+    public Color hitColor = Color.red;
+    public Color missColor = Color.green;
+    public LayerMask rayMask = ~0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawRaycast(transform.position, Vector3.down, rayLength, hitColor, missColor, rayMask);
+    }
+
+    // Performs a Physics.Raycast and draws a debug line: red if hit, green if miss
+    public void DrawRaycast(Vector3 origin, Vector3 direction, float length, Color hitColor, Color missColor, LayerMask mask)
+    {
+        Ray ray = new Ray(origin, direction.normalized);
+        RaycastHit hit;
+        
+        // Try to raycast, excluding this object's own collider
+        Collider thisCollider = GetComponent<Collider>();
+        
+        // Use OverlapSphere first to see what's in range
+        Collider[] colliders = Physics.OverlapSphere(origin + direction.normalized * (length / 2f), length / 2f, mask);
+        Debug.Log($"Colliders in range: {colliders.Length}");
+        foreach (var col in colliders)
+        {
+            Debug.Log($"  - {col.gameObject.name} (layer: {LayerMask.LayerToName(col.gameObject.layer)})");
+        }
+        
+        if (Physics.Raycast(ray, out hit, length, mask) && (thisCollider == null || hit.collider != thisCollider))
+        {
+            // Hit: draw from origin to contact point
+            Debug.Log($"HIT: {hit.collider.gameObject.name} at distance {hit.distance}");
+            Debug.DrawRay(origin, (hit.point - origin), hitColor);
+        }
+        else
+        {
+            // Miss: draw full ray length
+            Debug.Log("MISS - no colliders hit by raycast");
+            Debug.DrawRay(origin, direction.normalized * length, missColor);
+        }
+    }
+}
